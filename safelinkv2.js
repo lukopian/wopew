c46dc869570d09296f5fa4678defdf6ba0266ae4
lukopian <42808862+lukopian@users.noreply.github.com>
2018-09-09 08:53:19 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="2016/09/review-lenovo-vibe-x3.html"}
 if(x=="2"){xxx="2018/09/trading-in-forex-channel.html"}
 if(x=="3"){xxx="2018/09/the-basics-of-candlestick-charts-in.html"}
 if(x=="4"){xxx="2018/09/how-to-build-winning-trading-plan.html"}
-if(x=="5"){xxx="2018/09/the-basics-of-candlestick-charts-in_8.html"}a_to_ve[i].href="http://fox-software.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="2018/09/the-basics-of-candlestick-charts-in_8.html"}a_to_ve[i].href="https://fox-software.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
